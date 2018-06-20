1c6acb3cb39aaef6518c54ccfb7f1b426aacc725
romulotunala <romulotunala@gmail.com>
2018-06-20 13:02:20 -0300

    Fix things and Add slider

---
+++ main.js
@@ -0,0 +1,36 @@
+var change
+
+function resize(){
+	let element = document.querySelectorAll(".job__containerImages");
+	if(window.innerWidth <= '576'){
+		console.log('test', window.innerWidth)
+		let i;
+		for(i = 0;i < element.length;i++){
+			element[i].classList.add('slider');
+		}
+
+		let slideIndex = 1;
+		showDivs(slideIndex);
+
+		change = function plusDivs(n) {
+		    showDivs(slideIndex += n);
+		}
+
+		function showDivs(n) {
+		    let i;
+		    let x = document.getElementsByClassName('slider');
+		    if (n > x.length) {slideIndex = 1} 
+		    if (n < 1) {slideIndex = x.length} ;
+		    for (i = 0; i < x.length; i++) {
+		        x[i].style.display = "none"; 
+		    }
+		    x[slideIndex-1].style.display = "block";
+		}		
+	}else if(window.innerWidth > '576'){
+		let i;
+		for(i = 0;i < element.length;i++){
+			element[i].classList.remove('slider');
+			element[i].style.display = "block";
+		}
+	}
+}
