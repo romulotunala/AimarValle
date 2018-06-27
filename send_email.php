2eee42f3ce299985663a1b01f9db7f6b7249e3a7
romulotunala <romulotunala@gmail.com>
2018-06-27 09:30:22 -0300

    Send Email

---
+++ send_email.php
@@ -0,0 +1,30 @@
+<?php
+
+if(isset($_POST['enviar'])){
+	# alterar a variavel abaixo colocando o seu email
+	$recipient = "romulotunala@gmail.com";
+	$assunto = "solicitação de orçamento via site.";
+
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$text = $_POST['text'];
+
+
+	 // monta o e-mail na variavel $body
+
+	$body = "===================================" . "\n";
+	$body .= "FALE CONOSCO - TESTE COMPROVATIVO" . "\n";
+	$body .= "===================================" . "\n\n";
+	$body .= "Nome: " . $name . "\n";
+	$body .= "Email: " . $email . "\n";
+	$body .= "Mensagem: " . $text . "\n\n";
+	$body .= "===================================" . "\n";
+
+	// envia o email
+	mail($recipient, $assunto , $body, "From: $email\r\n");
+
+	// redireciona para a página de obrigado
+	header("location:contact.html");
+}
+
+?>
