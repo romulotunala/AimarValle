e38c206b15c1d8f6907192752bc37b6d320f7eea
romulotunala <romulotunala@gmail.com>
2018-07-09 18:30:46 -0300

    slider improvement

--- main.js
+++ main.js
@@ -27,9 +27,8 @@ function sliderON(){
 	    x[slideIndex-1].style.display = "block";
 	    x[slideIndex-1].style.width = '90%';
 	}
-
-	// document.getElementById('job__containerImages').style.width = '90%';
-	
+	document.getElementById('job__arrow--back').style.display = 'block';
+	document.getElementById('job__arrow--next').style.display = 'block';
 }
 
 function sliderOFF(){
@@ -39,64 +38,35 @@ function sliderOFF(){
 		element[i].style.display = "block";
 		element[i].style.width = "30%";
 	}
-	
+	document.getElementById('job__arrow--back').style.display = 'none';
+	document.getElementById('job__arrow--next').style.display = 'none';
 }
-
 
 function on_off(value) {
 	if (value === 'on') {
 		sliderON();
-		document.getElementById('job__arrow--back').style.display = 'block';
 		document.getElementById('job__arrow--back').style.top = '49%';
-		document.getElementById('job__arrow--next').style.display = 'block';
 		document.getElementById('job__arrow--next').style.top = '49%';
 	}else if(value === 'off'){
 		sliderOFF();
-		document.getElementById('job__arrow--back').style.display = 'none';
 		document.getElementById('job__arrow--back').style.top = '100px';
-		document.getElementById('job__arrow--next').style.display = 'none';
 		document.getElementById('job__arrow--next').style.top = '100px';
 	}
 };
 
 const codSlider = function (){
 	if(window.innerWidth <= '576'){
-		sliderON();
-		// let i;
-		// for(i = 0;i < element.length;i++){
-		// 	element[i].classList.add('slider');
-		// }
-
-		// showDivs(slideIndex);
-
-		// change = function plusDivs(n) {
-		//     showDivs(slideIndex += n);
-		// }
-
-		// function showDivs(n) {
-		//     let i;
-		//     let x = document.getElementsByClassName('slider');
-		//     if (n > x.length) {slideIndex = 1}; 
-		//     if (n < 1) {slideIndex = x.length};
-		//     for (i = 0; i < x.length; i++) {
-		//         x[i].style.display = "none";
-		//     }
-		//     x[slideIndex-1].style.display = "block";
-		// }		
+		sliderON();	
 	}else if(window.innerWidth > '576'){
 		sliderOFF();
-		// let i;
-		// for(i = 0;i < element.length;i++){
-		// 	element[i].classList.remove('slider');
-		// 	element[i].style.display = "block";
-		// }
 	}
 };
 
 
 codSlider();
 
-
 function resize(){
-	codSlider()
+	if (window.innerWidth <= '576') {
+			codSlider();
+	}
 };
