0c2160766d03ce78ddd5006720eea84c96fb1c29
romulotunala <romulotunala@gmail.com>
2018-08-06 19:55:13 -0300

    sliderShow finished

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let change;
-let element = document.querySelectorAll(".job__containerImages");
-let slideIndex = 1;
+let change,
+    element = document.querySelectorAll(".job__containerImages"),
+    slideIndex = 1;
 
 var selectSlider;
 
@@ -12,15 +12,18 @@ function sliderON(){
 
 	showDivs(slideIndex);
 
-	change = function plusDivs(n) {
+	change = function (n) {
 	    showDivs(slideIndex += n);
 	}
 
 	function showDivs(n) {
 	    let i;
 	    let x = document.getElementsByClassName('slider');
+
 	    if (n > x.length) {slideIndex = 1}; 
+
 	    if (n < 1) {slideIndex = x.length};
+
 	    for (i = 0; i < x.length; i++) {
 	        x[i].style.display = "none";
 	    }
@@ -35,22 +38,29 @@ function sliderOFF(){
 	let i;
 	for(i = 0;i < element.length;i++){
 		element[i].classList.remove('slider');
-		element[i].style.display = "block";
+		element[i].style.display = "flex";
 		element[i].style.width = "30%";
 	}
 	document.getElementById('job__arrow--back').style.display = 'none';
 	document.getElementById('job__arrow--next').style.display = 'none';
 }
 
-function on_off(value) {
+function on_off(value, imgPosition) {
 	if (value === 'on') {
+		if(imgPosition){
+			slideIndex = imgPosition;
+		}
 		sliderON();
 		document.getElementById('job__arrow--back').style.top = '49%';
 		document.getElementById('job__arrow--next').style.top = '49%';
+		document.getElementById('btn__slider--off').style.display = 'block';
+		document.getElementById('btn__slider--on').style.display = 'none';
 	}else if(value === 'off'){
 		sliderOFF();
 		document.getElementById('job__arrow--back').style.top = '100px';
 		document.getElementById('job__arrow--next').style.top = '100px';
+		document.getElementById('btn__slider--off').style.display = 'none';
+		document.getElementById('btn__slider--on').style.display = 'block';
 	}
 };
 
@@ -65,8 +75,53 @@ const codSlider = function (){
 
 codSlider();
 
+function removeLinkTop(){
+	var img = document.querySelectorAll('.job__containerImages');
+	
+	if(window.innerWidth <= '576'){
+		img.forEach(item =>{
+			item.classList.remove('linkTop');
+		})
+	}else if(window.innerWidth > '576'){
+		img.forEach(item =>{
+			item.classList.add('linkTop');
+		})
+	}
+}
+
+removeLinkTop();
+
 function resize(){
+	removeLinkTop();
+
 	if (window.innerWidth <= '576') {
-			codSlider();
+		codSlider();
+
 	}
 };
+
+// scroll
+
+const menuItems = document.querySelectorAll('.linkTop');
+
+
+menuItems.forEach(item => {
+	item.addEventListener('click', scrollToIdOnClick);
+})
+ 
+function scrollToIdOnClick(){
+
+	console.log('test')
+
+	// event.preventDefault();
+	// const element = event;
+	// console.log(element);
+
+	const id = '#container__imgs'
+	const to = document.querySelector(id).offsetTop;
+
+	window.scroll({
+		top:to,
+		behavior:'smooth',
+	});
+}
